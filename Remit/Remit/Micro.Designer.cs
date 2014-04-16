0e8605b3c5df0ff827e417abc9e2ecb0522656a0
W. Harkins <vdtdev@gmail.com>
2014-04-16 13:17:53 -0400

    Additional icons added

--- Remit/Remit/Micro.Designer.cs
+++ Remit/Remit/Micro.Designer.cs
@@ -31,17 +31,17 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Micro));
             this.layoutTable = new System.Windows.Forms.TableLayoutPanel();
-            this.btnReset = new System.Windows.Forms.Button();
             this.lHeadS = new System.Windows.Forms.Label();
             this.lHeadM = new System.Windows.Forms.Label();
             this.lHeadH = new System.Windows.Forms.Label();
             this.lSec = new System.Windows.Forms.Label();
             this.lDaysHourMin = new System.Windows.Forms.Label();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
             this.lHeadD = new System.Windows.Forms.Label();
             this.ticker = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.btnNew = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             this.layoutTable.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -76,17 +76,6 @@
             this.layoutTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.layoutTable.Size = new System.Drawing.Size(304, 77);
             this.layoutTable.TabIndex = 0;
-            // 
-            // btnReset
-            // 
-            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnReset.Image = global::Remit.Properties.Resources.remit_micro_icons_reset;
-            this.btnReset.Location = new System.Drawing.Point(3, 58);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(23, 15);
-            this.btnReset.TabIndex = 11;
-            this.btnReset.UseVisualStyleBackColor = true;
-            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // lHeadS
             // 
@@ -154,6 +143,23 @@
             this.lDaysHourMin.Text = "00:00:00";
             this.lDaysHourMin.TextAlign = System.Drawing.ContentAlignment.BottomRight;
             // 
+            // lHeadD
+            // 
+            this.lHeadD.AutoSize = true;
+            this.lHeadD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lHeadD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lHeadD.Location = new System.Drawing.Point(32, 0);
+            this.lHeadD.Name = "lHeadD";
+            this.lHeadD.Size = new System.Drawing.Size(61, 13);
+            this.lHeadD.TabIndex = 5;
+            this.lHeadD.Text = "days";
+            this.lHeadD.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // ticker
+            // 
+            this.ticker.Interval = 1000;
+            this.ticker.Tick += new System.EventHandler(this.ticker_Tick);
+            // 
             // btnStart
             // 
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
@@ -177,34 +183,28 @@
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
-            // lHeadD
-            // 
-            this.lHeadD.AutoSize = true;
-            this.lHeadD.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lHeadD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.lHeadD.Location = new System.Drawing.Point(32, 0);
-            this.lHeadD.Name = "lHeadD";
-            this.lHeadD.Size = new System.Drawing.Size(61, 13);
-            this.lHeadD.TabIndex = 5;
-            this.lHeadD.Text = "days";
-            this.lHeadD.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            // 
-            // ticker
-            // 
-            this.ticker.Interval = 1000;
-            this.ticker.Tick += new System.EventHandler(this.ticker_Tick);
-            // 
             // btnNew
             // 
             this.btnNew.AccessibleDescription = "Create new instance of Remit";
             this.btnNew.AccessibleName = "New Instance";
             this.btnNew.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnNew.Image = global::Remit.Properties.Resources.remit_micro_icons_reset;
+            this.btnNew.Image = global::Remit.Properties.Resources.remit_new;
             this.btnNew.Location = new System.Drawing.Point(287, 58);
             this.btnNew.Name = "btnNew";
             this.btnNew.Size = new System.Drawing.Size(14, 15);
             this.btnNew.TabIndex = 12;
             this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReset.Image = global::Remit.Properties.Resources.remit_micro_icons_reset;
+            this.btnReset.Location = new System.Drawing.Point(3, 58);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(23, 15);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // Micro
             // 
